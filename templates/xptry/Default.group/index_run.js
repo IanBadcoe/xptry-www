56be5514b68cfe94f25055ac278e064adda10e0e
Ian Badcoe <ian_badcoe@yahoo.co.uk>
2020-03-22 10:04:20 +0000

    moved HUD in front of content

--- templates/xptry/Default.group/index_run.js
+++ templates/xptry/Default.group/index_run.js
@@ -2,4 +2,7 @@ $(document).ready(() => {
     Draggable.Set($(".scroll-container"), [$(".background")]);
 
     window.SiteNav.Init("home-knot");
+
+    $(".hud-click").css("z-index", Zs.UI);
+    $(".hud-noclick").css("z-index", Zs.UI);
 });
---
+++ templates/xptry/Works.group/Zs.js
@@ -0,0 +1,10 @@
+"use strict";
+
+window.Zs = {
+	Decor:                  80,
+	BehindNodeContent:      90,
+	NodeContent:            100,
+	Strand:                 105,
+    InFrontOfNodeContent:   110,
+    UI:                     120
+};
