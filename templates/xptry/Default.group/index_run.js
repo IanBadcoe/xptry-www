a166ef5de3c3daba3f1339fde4168d987dc359bf
Ian Badcoe <ian_badcoe@yahoo.co.uk>
2020-03-22 12:22:39 +0000

    added do-nothing demand-loader to main page

--- templates/xptry/Default.group/index_run.js
+++ templates/xptry/Default.group/index_run.js
@@ -5,4 +5,19 @@ $(document).ready(() => {
 
     $(".hud-click").css("z-index", Zs.UI);
     $(".hud-noclick").css("z-index", Zs.UI);
+
+    function active_rect() {
+        let sc = $(".scroll-container");
+        let pos = sc.position();
+        pos = new Coord(pos.left, pos.top);
+
+        let dim = new Coord(innerWidth, innerHeight);
+
+        let rect = new Rect(pos.Inverse(),
+            pos.Inverse().Add(dim));
+
+        return rect.ExtendedBy(dim.Mult(0.5));
+    }
+
+    DemandLoader.Init(200, 300000, 5000, active_rect);
 });
