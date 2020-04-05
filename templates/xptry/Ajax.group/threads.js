d5da87c4fa9b2cf7f4e2f083f320d6e76fcf3a84
Ian Badcoe <ian_badcoe@yahoo.co.uk>
2020-04-05 15:24:52 +0000

    tidied threads AJAX template

--- templates/xptry/Ajax.group/threads.js
+++ templates/xptry/Ajax.group/threads.js
@@ -1,13 +1,13 @@
 {
-	{exp:channel:entries channel='thread' backspace='3' status='{segment_3}'}
-		"{url_title}" : {
-            "title" : "{title}",
-			"centre_x" : {centre_x},
-			"centre_y" : {centre_y},
-			"width" : {width},
-			"height" : {height},
-            "ctor" : "{ctor}",
-            "status" : "{status}"
-		},
-	{/exp:channel:entries}
+  {exp:channel:entries channel='thread' backspace='4' status='{segment_3}'}
+  "{url_title}" : {
+    "title" : "{title}",
+    "centre_x" : {centre_x},
+    "centre_y" : {centre_y},
+    "width" : {width},
+    "height" : {height},
+    "ctor" : "{ctor}",
+    "status" : "{status}"
+    },
+  {/exp:channel:entries}
 }
