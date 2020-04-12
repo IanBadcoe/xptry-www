a85218c7c3b91724bb906f46924b2ae34eba421f
Ian Badcoe <ian_badcoe@yahoo.co.uk>
2020-04-12 15:14:12 +0000

    removed obsolete/irrelevant test page

--- templates/xptry/Test.group/torus_test.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-    add_holed_circle($(".test-line"), 250, 178,
-        "stroke-width:0;", "rotating", "/upload/resources/infrastructure/Home_Knot.png", "bob");
-});
