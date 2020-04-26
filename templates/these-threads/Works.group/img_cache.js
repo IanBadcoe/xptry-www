a2cd2dc0d5cd583c1e2f648ae68be1760debade7
Ian Badcoe <ian_badcoe@yahoo.co.uk>
2020-04-26 20:04:57 +0000

    fixed a problem with ImageCache where 304s were considered errors and rejected the promise

--- templates/these-threads/Works.group/img_cache.js
+++ templates/these-threads/Works.group/img_cache.js
@@ -2,14 +2,33 @@
 
 $(document).ready(() => {
     let cache = {};
+    let bbg = $(".behind-background");
 
     window.ImageCache = {
         // returns a promise, so we can queue-up storing the reference
         Preload(path) {
             let record = {}
 
-            record.element = $("<img/>").attr("src", path);
-            record.promise = record.element.promise("load");
+            let el = new Image();
+            record.element = $(el);
+            record.element.addClass("zero-spacing");
+
+            // make sure it is actually loaded, so it will have a width
+            bbg.append(record.element);
+
+            // when I tried this in jQuery, if we got a 304 then the promise failed
+            // which meant when I was waiting for collections of promises I lost time synchronisation
+            // will still need to handle real errors on that...
+            record.promise = new Promise((res, rej) => {
+                el.onload = res;
+                el.onerror = rej;
+                el.src = path;
+            }).then(() => {
+                record.width = record.element.width();
+                record.height = record.element.height();
+                record.element.remove();
+                //console.log("ImageCache Loaded: " + path);
+            });
 
             cache[path] = record;
 
@@ -22,6 +41,12 @@ $(document).ready(() => {
         // returns a clone of the cached elephant
         Element(path) {
             return cache[path].element.clone();
+        },
+        Dims(path) {
+            return new Coord(cache[path].width, cache[path].height);
+        },
+        Clear() {
+            cache = {};
         }
     };
 });
