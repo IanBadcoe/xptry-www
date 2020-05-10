53c405baa9c74021fc99da9f1dede947c3f7f6bf
Ian Badcoe <ian_badcoe@yahoo.co.uk>
2020-05-10 10:41:10 +0000

    reworked draggable persp to create perspective planes on demand

--- templates/these-threads/Test.group/persp_run.js
+++ templates/these-threads/Test.group/persp_run.js
@@ -1,16 +1,28 @@
 $(document).ready(() => {
-    DraggablePersp.Set([
-        {
-            element: $(".scroll-container"),
-            dist: 1.0
-        },
-        {
-            element: $(".scroll-container2"),
-            dist: 2.0
-        },
-        {
-            element: $(".scroll-container10"),
-            dist: 10.0
-        },
-    ]);
+    DraggablePersp.Init($(".scroll-container-container"), $("body"));
+    let p10 = DraggablePersp.Get(10.0);
+    let p1 = DraggablePersp.Get(1.0);
+    let p2 = DraggablePersp.Get(2.0);
+
+    let fn = function(i, j) {
+        let d = $("<div></div>")
+            .addClass("absolute zero-spacing")
+            .css({
+                left: i,
+                top: j,
+                width: 90,
+                height: 90,
+                "background-color": "rgb(" + Math.random() * 255 + "," + Math.random() * 255 + "," + Math.random() * 255 + ")"
+            });
+
+        return d;
+    }
+
+    for(let i = -500; i < 1500; i += 100) {
+        for(let j = -500; j < 1500; j += 100) {
+            p1.append(fn(i, j));
+            p2.append(fn(i, j));
+            p10.append(fn(i, j));
+        }
+    }
 });
--- templates/these-threads/Works.group/draggable_persp.js
+++ templates/these-threads/Works.group/draggable_persp.js
@@ -1,67 +1,106 @@
 "use strict";
 
 $(document).ready(() => {
-    let md = false;
-    let click_pos = null;
-    let main_target = null;
-    let other_targets = null;
-    let old_pos = null;
+    let _md = false;
+    let _click_pos = null;
+    let _main_target = null;
+    let _all_targets = [];
+    let _old_pos = null;
+    let _container = null;
+    let _mouse_target = null;
+
+    function mousedown(e) {
+        if (!_md) {
+            _md = true;
+            _click_pos = new Coord(e.pageX, e.pageY);
+            let pos = _main_target.position();
+            _old_pos = new Coord(pos.left, pos.top);
+        }
+
+        return false;
+    }
+
+    function mousemove(e) {
+        if (_md) {
+            let move = new Coord(e.pageX, e.pageY).Sub(_click_pos);
+            let main_pos = _old_pos.Add(move);
+
+            _all_targets.forEach(x => {
+                x.element.css({
+                    left: main_pos.X / x.dist,
+                    top: main_pos.Y
+                });
+            });
+
+            $(".hack").toggle();
+            
+            return false;
+        }
+    }
+
+    function mouseup(e) {
+        _md = false;
+
+        return false;
+    }
 
     window.DraggablePersp = {
-        // targets is an array:
-        // [
-        //     {
-        //          element: $(...),
-        //          dist: 1.0     
-        //     },
-        //     ...
-        // ]
+        // assumptions:
+        // 1) we initialise with the main perspective plane, which is at relative distance: 1.0
+        // 1a) and mouse_target, which  is the only element that needs to handle mouse events
+        // 2) the initial position of all planes is acceptable at (0, 0)
+        //    ( if that wasn't the case, then we'd need to record the start pos of all targets
+        //      and add/subtract those when going between the main and other targets )
+        Init(container, mouse_target) {
+            _container = container;
+            _mouse_target = mouse_target;
 
-        // we are assuming here, that the initial position of all the elements is (0, 0)
-        // if that isn't the case, then we need to record the start pos of all targets
-        // and add/subtract those when going between the main and other targets
-        Set(targets) {
-            main_target = targets[0];
-            other_targets = targets.slice(1);
+            _mouse_target.on("mousedown", mousedown);
+            _mouse_target.on("mouseup", mouseup);
+            _mouse_target.on("mousemove", mousemove);
 
-            function mousedown(e) {
-                if (!md) {
-                    md = true;
-                    click_pos = new Coord(e.pageX, e.pageY);
-                    let pos = main_target.element.position();
-                    old_pos = new Coord(pos.left, pos.top);
+            _main_target = this.Get(1.0);
+        },
+        Get(dist) {
+            let ins_before = 0;
+            let found = null;
+
+            _all_targets.forEach(x => {
+                if (x.dist === dist) {
+                    found = x.element;
                 }
-
-                return false;
-            }
-
-            function mousemove(e) {
-                if (md) {
-                    let move = new Coord(e.pageX, e.pageY).Sub(click_pos);
-                    let main_pos = old_pos.Add(move);
-
-                    [main_target, ...other_targets].forEach(x => {
-                        x.element.css({
-                            left: main_pos.X / x.dist,
-                            top: main_pos.Y
-                        });
-                    });
-
-                    $(".hack").toggle();
-                    
-                    return false;
+                if(x.dist > dist) {
+                    ins_before++;
                 }
+            });
+
+            if (found)
+                return found;
+
+            let klass = ("scroll-container-" + dist).replace(".", "-");
+
+            let el = $("<div></div>")
+                .addClass("scroll-container zero-spacing")
+                .addClass(klass)
+                .css({
+                    transform: "scale(" + 1 / dist + ")"
+                });
+
+            let ent = {
+                element: el,
+                dist: dist,
+                klass: klass
+            };
+
+            if (!_all_targets.length) {
+                _container.append(el);
+            } else {
+                _container.children().eq(ins_before).before(el);
             }
 
-            function mouseup(e) {
-                md = false;
+            _all_targets.splice(ins_before, 0, ent);
 
-                return false;
-            }
-
-            main_target.element.on("mousedown", mousedown);
-            main_target.element.on("mouseup", mouseup);
-            main_target.element.on("mousemove", mousemove);
+            return ent.element;
         }
     };
 });
