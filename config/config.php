f8a822bc19e3d3d5588e85a874c14ed1c44779ca
Ian Badcoe <ian_badcoe@yahoo.co.uk>
2020-12-09 17:25:12 +0000

    upgrade EE

--- config/config.php
+++ config/config.php
@@ -9,7 +9,7 @@ $config['show_ee_news'] = 'n';
 // Find more configs and overrides at
 // https://docs.expressionengine.com/latest/general/system_configuration_overrides.html
 
-$config['app_version'] = '5.3.0';
+$config['app_version'] = '5.4.0';
 $config['encryption_key'] = '7d37828f483edf234986b751959b0323361f26e1';
 $config['session_crypt_key'] = '0f89278163a298bed9d9298800dffcd36c6fa7fa';
 $config['database'] = array(
